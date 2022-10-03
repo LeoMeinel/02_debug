e624280001f054a87c8d1f07b15d2901f2eb5381
Leopold Johannes Meinel <leo@meinel.dev>
2022-10-04 00:43:43 +0200

    Give jvm more memory
    - This is to make debugging easier

--- java/papermc/velocity/velocity.sh
+++ java/papermc/velocity/velocity.sh
@@ -11,4 +11,4 @@
 
 set -eu
 
-java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
+java -Xms4G -Xmx4G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
