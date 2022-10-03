fccdc9447f2358bffd7601f0d0b114f73324daba
Leopold Johannes Meinel <leo@meinel.dev>
2022-10-04 00:16:20 +0200

    Initial commit

---
+++ java/papermc/update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+###
+# File: update.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2022 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
+
+set -eu
+source ./variables
+
+PAPER_BUILD="$(/usr/bin/curl -s "https://papermc.io/api/v2/projects/paper/versions/${PAPER_VERSION}" | /usr/bin/grep -Po 'builds.*' | /usr/bin/awk -F ',' '{ print $NF }' | /usr/bin/grep -Po '\d+')"
+curl -s https://papermc.io/api/v2/projects/paper/versions/$PAPER_VERSION/builds/$PAPER_BUILD/downloads/paper-$PAPER_VERSION-$PAPER_BUILD.jar >"$PAPER_TARGET"/paper.jar
+VELOCITY_BUILD="$(/usr/bin/curl -s "https://papermc.io/api/v2/projects/velocity/versions/${VELOCITY_VERSION}" | /usr/bin/grep -Po 'builds.*' | /usr/bin/awk -F ',' '{ print $NF }' | /usr/bin/grep -Po '\d+')"
+curl -s https://papermc.io/api/v2/projects/velocity/versions/$VELOCITY_VERSION/builds/$VELOCITY_BUILD/downloads/velocity-$VELOCITY_VERSION-$VELOCITY_BUILD.jar >"$VELOCITY_TARGET"/velocity.jar
---
+++ java/papermc/velocity/velocity.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###
+# File: velocity.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2022 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
+
+set -eu
+
+java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
